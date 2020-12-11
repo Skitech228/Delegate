4f2b4c0cb35d05cc83b9ec3aa706d909f0d96617
1183764 <1183764@K408-10>
2020-12-11 10:58:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        delegate double[] func(int a, int b);
+        static void Main(string[] args)
+        {
+            РисовательТаблиц рисователь = new РисовательТаблиц();
+            Console.WriteLine("Введите а,б");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            func funct = рисователь.Sinn;
+            funct += рисователь.SinAndCos;
+            funct += рисователь.x;
+            double[] u;
+            foreach (var item in funct.GetInvocationList())
+            {
+                u=(double[])item.DynamicInvoke(a, b);
+                for (int i = 0; i < u.Length; i++)
+                {
+                    Console.Write(u[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp36/РисовательТаблиц.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    class РисовательТаблиц
+    {
+        public double[] Sinn(int a, int b)
+        { int c=0;
+            if (a > b)
+                c = a - b;
+            else
+                c = b - a;
+            double[] sinnys = new double[c];
+            c = a;
+            for (int i = 0; i < sinnys.Length; i++)
+            {
+                sinnys[i] = Math.Round(Math.Sin(c *Math.PI/180), 3);
+                c++;
+            }
+            return sinnys;
+        }
+
+        public double[] SinAndCos(int a, int b)
+        {
+            int c = 0;
+            if (a > b)
+                c = a - b;
+            else
+                c = b - a;
+            double[] sinnys = new double[c];
+            double u = a;
+            for (int i = 0; i < sinnys.Length; i++)
+            {
+                sinnys[i] = (Math.Pow(Math.Sin(u * 180 / Math.PI),2) + Math.Pow(Math.Cos(u * 180 / Math.PI), 2));
+                u++;
+            }
+            return sinnys;
+        }
+
+        public double[] x(int a, int b)
+        {
+            int c = 0;
+            if (a > b)
+                c = a - b;
+            else
+                c = b - a;
+            double[] sinnys = new double[c];
+            c = a;
+            for (int i = 0; i < sinnys.Length; i++)
+            {
+                try
+                {
+                    sinnys[i] = Math.Round((double)1/c,3);
+                }
+
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    sinnys[i] = 0;
+                }
+                c++;
+            }
+            return sinnys;
+        }
+    }
+}
